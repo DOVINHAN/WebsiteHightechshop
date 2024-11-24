fa0cf5bad38f2cf9f447e221048abedb1406dd5f
TronkIsHere <nguyenhuutrong11133@gmail.com>
2024-11-25 03:20:31 +0700

    Add banner2, services, footer component

--- front-end/src/components/HomePageComponent/Banner2/Banner2.jsx
+++ front-end/src/components/HomePageComponent/Banner2/Banner2.jsx
@@ -1,7 +1,197 @@
 import React from "react";
+import Heading from "../../shared/Heading";
+import { FaArrowRightLong } from "react-icons/fa6";
+import img1 from "../../../assets/banner2/img1.png";
+import img2 from "../../../assets/banner2/img2.png";
+import img3 from "../../../assets/banner2/img3.png";
+import img4 from "../../../assets/banner2/img4.png";
 
 const Banner2 = () => {
-  return <div>Banner2</div>;
+  return (
+    <div className="mt-20">
+      <div className="container">
+        <Heading title={"SẢN PHẨM MỚI"} />
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5 text-white">
+          {/* column1 */}
+          <div className="flex h-full bg-black p-8 rounded-lg shadow-md">
+            {/* content */}
+            <div className="w-2/5  flex flex-col justify-center p-8">
+              <h1
+                data-aos="fade-down"
+                data-aos-delay="200"
+                className="text-white font-bold text-3xl"
+              >
+                IPHONE 16 PROMAX
+              </h1>
+              <h2
+                data-aos="fade-right"
+                data-aos-delay="250"
+                className="text-md text-white my-5"
+              >
+                Phiên bản xanh của IPHONE16 PROMAX sắp được bán.
+              </h2>
+              <button
+                data-aos="fade-up"
+                data-aos-delay="300"
+                className="self-start text-white inline-flex items-center"
+                style={{
+                  textDecoration: "underline",
+                  textDecorationThickness: "1px",
+                  textUnderlineOffset: "5px",
+                }}
+              >
+                Mua ngay <FaArrowRightLong className="ml-2" />
+              </button>
+            </div>
+            {/* img */}
+            <div className="w-3/5">
+              <img
+                data-aos="fade-left"
+                data-aos-delay="100"
+                src={img1}
+                className=" h-full max-[450px]: object-cover"
+              />
+            </div>
+          </div>
+
+          {/* column2 */}
+          <div className="grid grid-rows-2 gap-5">
+            {/* half column2 */}
+            <div className="flex bg-black p-8 rounded-lg shadow-md">
+              {/* content */}
+              <div className="w-1/2 flex flex-col justify-center p-8">
+                <h1
+                  data-aos="fade-up"
+                  data-aos-delay="200"
+                  className="text-white font-bold text-3xl"
+                >
+                  Đồng hồ thông minh
+                </h1>
+                <h2
+                  data-aos="fade-left"
+                  data-aos-delay="250"
+                  className="text-md text-white my-5"
+                >
+                  Các bộ sưu tập dành cho phụ nữ nổi bật mang đến cho bạn cảm
+                  giác khác.
+                </h2>
+                <button
+                  data-aos="fade-down"
+                  data-aos-delay="300"
+                  className="self-start text-white inline-flex items-center"
+                  style={{
+                    textDecoration: "underline",
+                    textDecorationThickness: "1px",
+                    textUnderlineOffset: "5px",
+                  }}
+                >
+                  Mua ngay <FaArrowRightLong className="ml-2" />
+                </button>
+              </div>
+              {/* img */}
+              <div className="w-1/2">
+                <img
+                  data-aos="fade-right"
+                  data-aos-delay="100"
+                  src={img2}
+                  className="object-cover"
+                />
+              </div>
+            </div>
+
+            {/* half column2 */}
+            <div className="grid grid-cols-2 gap-5">
+              {/* column1 */}
+              <div className="bg-black p-8 rounded-lg shadow-md relative">
+                {/* content */}
+                <div className="absolute bottom-8 left-8 right-8 text-center z-10">
+                  <h1
+                    data-aos="fade-up"
+                    data-aos-delay="200"
+                    className="text-white font-bold text-3xl"
+                  >
+                    Tai nghe bluetooth
+                  </h1>
+                  <h2
+                    data-aos="fade-up"
+                    data-aos-delay="250"
+                    className="text-md text-white my-5"
+                  >
+                    Mẫu tai nghe mới nhất.
+                  </h2>
+                  <button
+                    data-aos="fade-up"
+                    data-aos-delay="300"
+                    className="self-center text-white inline-flex items-center"
+                    style={{
+                      textDecoration: "underline",
+                      textDecorationThickness: "1px",
+                      textUnderlineOffset: "5px",
+                    }}
+                  >
+                    Mua ngay <FaArrowRightLong className="ml-2" />
+                  </button>
+                </div>
+
+                {/* img */}
+                <div className="w-full flex justify-center mt-8">
+                  <img
+                    data-aos="fade-down"
+                    data-aos-delay="100"
+                    src={img3}
+                    className="object-cover max-w-[300px] max-h-[300px]"
+                  />
+                </div>
+              </div>
+
+              {/* column2 */}
+              <div className="bg-black p-8 rounded-lg shadow-md relative">
+                {/* content */}
+                <div className="absolute bottom-8 left-8 right-8 text-center z-10">
+                  <h1
+                    data-aos="fade-up"
+                    data-aos-delay="200"
+                    className="text-white font-bold text-3xl"
+                  >
+                    IPAD mới nhất
+                  </h1>
+                  <h2
+                    data-aos="fade-up"
+                    data-aos-delay="250"
+                    className="text-md text-white my-5"
+                  >
+                    Mẫu ipad mới nhất.
+                  </h2>
+                  <button
+                    data-aos="fade-up"
+                    data-aos-delay="300"
+                    className="self-center text-white inline-flex items-center"
+                    style={{
+                      textDecoration: "underline",
+                      textDecorationThickness: "1px",
+                      textUnderlineOffset: "5px",
+                    }}
+                  >
+                    Mua ngay <FaArrowRightLong className="ml-2" />
+                  </button>
+                </div>
+
+                {/* img */}
+                <div className="w-full flex justify-center mt-8">
+                  <img
+                    data-aos="fade-down"
+                    data-aos-delay="100"
+                    src={img4}
+                    className="object-cover max-w-[300px] max-h-[300px]"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Banner2;
--- front-end/src/components/HomePageComponent/Services/Services.jsx
+++ front-end/src/components/HomePageComponent/Services/Services.jsx
@@ -1,7 +1,53 @@
 import React from "react";
+import { FaCarSide } from "react-icons/fa";
+import { TfiHeadphoneAlt } from "react-icons/tfi";
+import { FaShield } from "react-icons/fa6";
 
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <div className="mt-40 container">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+        {/* Giao hàng miễn phí */}
+        <div className="text-center">
+          <div className="bg-gray-400 inline-flex items-center justify-center p-3 rounded-full mb-8">
+            <div className="bg-black p-4 rounded-full">
+              <FaCarSide className="text-white text-3xl" />
+            </div>
+          </div>
+          <div className="font-bold text-lg">
+            GIAO HÀNG MIỄN PHÍ VÀ NHANH CHÓNG
+          </div>
+          <div className="text-gray-500">
+            Giao hàng miễn phí cho tất cả các đơn hàng trên 500.000vnđ
+          </div>
+        </div>
+
+        {/* Dịch vụ khách hàng */}
+        <div className="text-center">
+          <div className="bg-gray-400 inline-flex items-center justify-center p-3 rounded-full mb-8">
+            <div className="bg-black p-4 rounded-full">
+              <TfiHeadphoneAlt className="text-white text-3xl" />
+            </div>
+          </div>
+          <div className="font-bold text-lg">DỊCH VỤ KHÁCH HÀNG 24/7</div>
+          <div className="text-gray-500">Hỗ trợ khách hàng thân thiện 24/7</div>
+        </div>
+
+        {/* Đảm bảo hoàn tiền */}
+        <div className="text-center">
+          <div className="bg-gray-400 inline-flex items-center justify-center p-3 rounded-full mb-8">
+            <div className="bg-black p-4 rounded-full">
+              <FaShield className="text-white text-3xl" />
+            </div>
+          </div>
+          <div className="font-bold text-lg">ĐẢM BẢO HOÀN TIỀN</div>
+          <div className="text-gray-500">
+            Chúng tôi đảm bảo hoàn lại tiền trong vòng 30 ngày
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Services;
